de636cc053ab76a66ada4bead4483df111f32582
Tan Ga Foo <chevaliertann@gmail.com>
2025-05-17 20:38:29 +0800

    Add fonts and update home page colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,32 @@
-<section class="grid grid-cols-6 gap-3 p-3">
-	<div class="card col-span-4 h-64 border-3">
-		<div class="h-12 w-full border-b-3 border-black"></div>
+<section class="grid grid-cols-6 gap-5 p-3">
+	<div class="card col-span-4 max-h-fit border-3">
+		<div class="flex h-12 w-full items-center border-b-3 border-black bg-zinc-400 px-2">
+			<h1 class="font-pixelify text-2xl text-zinc-50">
+				<span class="bg-red-500 px-1 font-sans font-bold text-white">MARVEL</span>
+				needs Kevin Feige !
+			</h1>
+		</div>
+		<div class="flex flex-col gap-2 bg-zinc-300 p-2.5">
+			<div class="chat chat-start">
+				<div
+					class="chat-bubble font-ballet flex gap-4 pt-6 text-3xl font-bold text-pretty shadow-md"
+				>
+					"Thunderbolts* is the best movie Marvel has put out in awhile"
+				</div>
+			</div>
+			<div class="chat chat-end">
+				<div class="chat-bubble chat-bubble-info flex gap-4 shadow-md">
+					<input
+						class="input chat-bubble !rounded-box flex-1 bg-white text-black shadow-md"
+						placeholder="Message for Kevin Feige"
+					/>
+					<button
+						class="btn btn-secondary rounded-box font-pixelify via-secondary bg-gradient-to-t from-cyan-500 to-cyan-500 text-base shadow-md uppercase hover:scale-105 transition-all ease-in"
+						>Send</button
+					>
+				</div>
+			</div>
+		</div>
 	</div>
 	<div class="card col-span-2 border-3 border-black">
 		<div class="card bg-base-100 shadow-sm">
@@ -11,13 +37,16 @@
 				/>
 			</figure>
 			<div class="card-body">
-				<h2 class="card-title">Card Title</h2>
 				<p>
 					A card component has a figure, a body part, and inside body there are title and actions
 					parts
 				</p>
 				<div class="card-actions justify-end">
-					<button class="btn btn-primary">Buy Now</button>
+					<button
+						class="font-pixelify max-w-fit rounded-full border-2 border-amber-500 bg-gradient-to-br from-amber-500 via-yellow-200 to-amber-500 px-6 py-1 text-lg font-semibold italic shadow-md"
+					>
+						Donate
+					</button>
 				</div>
 			</div>
 		</div>
